b36c71ea055c0c9cc64e83c425758fe0387fa578
TaylorHo <taylor@hotay.dev>
2024-02-06 20:36:57 +0000

    wip: init configuration file

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,6 @@
 import { BaseCommand } from "../base-command.js";
+import path from "node:path";
+import { writeFile } from "node:fs";
 
 export default class Init extends BaseCommand {
   static description = "Configure the tool - long description";
@@ -8,5 +10,26 @@ export default class Init extends BaseCommand {
 
   public async run(): Promise<void> {
     // Configure the tool with an .enygmah file and a folder for hooks and other configuration
+    const config = {
+      module: true,
+      eslint: true,
+      prettier: true,
+      hook: true,
+      quality: true,
+      security: true,
+      severity: ["informative", "low", "medium", "high", "critical"],
+    };
+    // TODO: Save the config file with a different name based on the dev/prod environment, for test purposes
+    writeFile(
+      path.join(process.cwd(), ".enygmah"),
+      JSON.stringify(config, null, 2),
+      "utf-8",
+      (error) => {
+        if (error) {
+          this.error(error);
+        }
+        this.log("Configured with success!");
+      },
+    );
   }
 }
