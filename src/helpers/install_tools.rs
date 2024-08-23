58474f4a9591ce1a61c13ca240f26cf08236b78e
TaylorHo <taylor@hotay.dev>
2024-08-23 20:54:07 -0300

    feat: pulling docker image

--- src/helpers/install_tools.rs
+++ src/helpers/install_tools.rs
@@ -5,26 +5,28 @@ use bollard::{
 };
 
 use futures_util::StreamExt;
-use log::{debug, error, info, trace, warn};
-use std::process;
+use indicatif::{HumanBytes, MultiProgress, ProgressBar, ProgressStyle};
+use log::{debug, error, info};
+use std::{collections::HashMap, process};
 
 pub async fn install_tools() {
-    // Progress bars: https://docs.rs/indicatif/latest/indicatif/
-
-    // Here we need to download the Docker image containing all the tools and run the container
-    // https://github.com/fussybeaver/bollard?tab=readme-ov-file
-
     let docker: Docker = match Docker::connect_with_local_defaults() {
         Ok(res) => {
             debug!("Docker instance: {:#?}", res);
             res
         }
         Err(err) => {
-            // TODO: handle each case apart
-            error!(
-                "Ensure that Docker is installed and running.\nError: {}",
-                err
-            );
+            match err {
+                bollard::errors::Error::SocketNotFoundError(_) => {
+                    error!("It wasn't possible to connect to Docker Socket. Ensure that Docker is installed and running.");
+                }
+                _ => {
+                    error!(
+                        "An error occurred trying to connect to docker socket: {}",
+                        err
+                    );
+                }
+            }
             process::exit(1);
         }
     };
@@ -43,20 +45,61 @@ pub async fn install_tools() {
             id
         }
         None => {
-            warn!("enygmah docker image not found, trying to pull...");
+            println!("enygmah docker image not found, pulling...");
 
             let pull_options = Some(CreateImageOptions {
                 from_image: "hotay/enygmah",
-                // tag: "latest",
+                tag: "latest",
                 ..Default::default()
             });
 
             let mut stream = docker.create_image(pull_options, None, None);
 
+            // Initialize MultiProgress to manage multiple progress bars
+            let multi_progress = MultiProgress::new();
+            let progress_style: ProgressStyle = ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] {wide_bar:.cyan/blue} {msg}")
+                .unwrap()
+                .progress_chars("##-");
+
+            // HashMap to store progress bars by id
+            let mut progress_bars: HashMap<String, (ProgressBar, i64, i64)> = HashMap::new();
+
             while let Some(result) = stream.next().await {
                 match result {
                     Ok(create_image_info) => {
-                        trace!("Docker pull update: {:?}", create_image_info);
+                        if let Some(id) = &create_image_info.id {
+                            if let Some(progress_detail) = &create_image_info.progress_detail {
+                                let current = progress_detail.current.unwrap_or(0);
+                                let total = progress_detail.total.unwrap_or(0);
+
+                                if total != 0 && total != 0 {
+                                    // Get or create a progress bar for this id
+                                    let (progress_bar, _, _) =
+                                        progress_bars.entry(id.clone()).or_insert_with(|| {
+                                            let pb =
+                                                multi_progress.add(ProgressBar::new(total as u64));
+                                            pb.set_style(progress_style.clone());
+                                            pb.set_message(format!("layer {}", id.clone()));
+                                            (pb, 0, 0)
+                                        });
+
+                                    // Update the progress bar with the current value
+                                    progress_bar.set_position(current as u64);
+                                    progress_bar.set_length(total as u64);
+
+                                    if current == total {
+                                        progress_bar.finish_with_message("Done");
+                                    } else {
+                                        progress_bar.set_message(format!(
+                                            "{}/{}",
+                                            HumanBytes(current as u64),
+                                            HumanBytes(total as u64)
+                                        ));
+                                    }
+                                }
+                            }
+                        }
                     }
                     Err(e) => {
                         error!("Failed pulling enygmah docker image. Error logs:\n{}", e);
@@ -64,6 +107,11 @@ pub async fn install_tools() {
                     }
                 }
             }
+
+            multi_progress
+                .clear()
+                .expect("Failed removing progress bars...");
+            println!("Docker image pulled!");
 
             let updated_images_list = &docker
                 .list_images(Some(ListImagesOptions::<String> {
