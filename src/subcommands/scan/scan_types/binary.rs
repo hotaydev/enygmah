f571f3d63545abff1b9acb1a04360c03506519be
TaylorHo <taylor@hotay.dev>
2024-09-16 14:13:54 -0300

    chore: default messages for unimplemented analysis types

--- src/subcommands/scan/scan_types/binary.rs
+++ src/subcommands/scan/scan_types/binary.rs
@@ -1,3 +1,14 @@
+use crate::helpers::logger;
+use log::debug;
+
 pub async fn analyze(path: &String) {
-    println!("{}", path);
+    debug!("{}", path);
+    logger::create_log(
+        "We are currently only analyzing repositories (local and remote) and web applications. This analysis will be implemente soon.",
+        logger::EnygmahLogType::Warn,
+    );
+    logger::create_log(
+        "To be up-to-date with improvements on this tool, check https://github.com/hotaydev/enygmah",
+        logger::EnygmahLogType::Info,
+    );
 }
--- src/subcommands/scan/scan_types/docker_image.rs
+++ src/subcommands/scan/scan_types/docker_image.rs
@@ -1,3 +1,14 @@
-pub async fn analyze(image: &String) {
-    println!("{}", image);
+use crate::helpers::logger;
+use log::debug;
+
+pub async fn analyze(path: &String) {
+    debug!("{}", path);
+    logger::create_log(
+        "We are currently only analyzing repositories (local and remote) and web applications. This analysis will be implemente soon.",
+        logger::EnygmahLogType::Warn,
+    );
+    logger::create_log(
+        "To be up-to-date with improvements on this tool, check https://github.com/hotaydev/enygmah",
+        logger::EnygmahLogType::Info,
+    );
 }
