1aca499b15dc51ce8f3f14f57454e45f9f542e40
TaylorHo <taylor@hotay.dev>
2024-12-18 13:36:16 -0300

    feat: downloading results after deleting content from container

--- src/subcommands/scan/hooks/post_scan.rs
+++ src/subcommands/scan/hooks/post_scan.rs
@@ -1,11 +1,33 @@
-use bollard::Docker;
+use bollard::{container::DownloadFromContainerOptions, Docker};
+use futures_util::StreamExt;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::helpers::enygmah_docker::{self, get_docker};
 
-// TODO: get results before cleaning up the _outputs folder
 pub async fn clean_up() {
-    let _docker: Docker = get_docker();
-    // enygmah_docker::execute_command(&docker, String::from("rm -rf /home/enygmah/_outputs/")).await;
+    let docker: Docker = get_docker();
+
+    // TODO: Instead of downloading the tarball, we should add this to a local .enygmah folder and serve the results in a web server.
+    let mut file: File = File::create("analysis-results.tar").await.unwrap();
+
+    // Download the content from the container as a stream
+    let mut stream = docker.download_from_container(
+        "enygmah",
+        Some(DownloadFromContainerOptions {
+            path: "/home/enygmah/_outputs/",
+        }),
+    );
+
+    // Write the stream to the file
+    while let Some(chunk) = stream.next().await {
+        let data = chunk.unwrap();
+        file.write_all(&data).await.unwrap();
+    }
+
+    // Ensure the file is properly flushed
+    file.flush().await.unwrap();
+
+    enygmah_docker::execute_command(&docker, String::from("rm -rf /home/enygmah/_outputs/")).await;
 }
 
 pub async fn delete_created_folder(docker: &Docker, folder: &str) {
